4032038ff4ef2dd536a1ad2b6c90cd4d039f275f
aniwhjr <77626878+aniwhjr@users.noreply.github.com>
2021-03-08 20:00:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var ball;
+
+var position;
+var database;
+
+function setup() {
+  createCanvas(500, 500);
+  database = firebase.database();
+  var locofnode = database.ref("ball/position");
+  locofnode.on("value", readposition, showerr);
+  ball = createSprite(250, 250, 10, 10);
+  ball.shapeColor = "red";
+}
+
+function draw() {
+  background("white");
+  if (keyDown(LEFT_ARROW)) {
+    changePosition(-1, 0);
+  } else if (keyDown(RIGHT_ARROW)) {
+    changePosition(1, 0);
+  } else if (keyDown(UP_ARROW)) {
+    changePosition(0, -1);
+  } else if (keyDown(DOWN_ARROW)) {
+    changePosition(0, +1);
+  }
+  drawSprites();
+}
+
+function writePosition(x, y) {
+  database.ref("ball/position").set({ x: ball.x + x, y: ball.y + y });
+}
+
+function readposition(data) {
+  position = data.val();
+  ball.x = position.x;
+  ball.y = position.y;
+}
+
+function showerr() {
+  console.log("Error in reading through the database");
+}
